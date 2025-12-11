52ff5c5f83bdea7f1ec94143fcaa7622de3ba0a5
ChernovaNatalya <natali-4ch@yandex.ru>
2025-12-11 18:45:07 +0300

    try1

--- src/shared/BasketCard/index.tsx
+++ src/shared/BasketCard/index.tsx
@@ -9,65 +9,70 @@ import { IPizza } from '../interfaces';
 
 interface ICard {
   name: string;
-  image?: string| undefined;
-  href?:string | undefined;
+  image?: string | undefined;
+  href?: string | undefined;
   cost: number;
   sale?: number | undefined;
-  currency:string;
+  currency: string;
   amount: number;
-  changeAmount:(id:number,new_amount: number)=>void;
+  changeAmount: (id: number, new_amount: number) => void;
   id: number;
 }
 
-const BasketCard= ({
+const BasketCard = ({
   name,
   image,
   href,
   cost,
   sale,
-  currency, 
+  currency,
   changeAmount,
   amount,
   id,
-}:ICard) => {
+}: ICard) => {
 
-  const decrease = ()=>{
+  const decrease = () => {
     console.log('-----');
-    changeAmount(id,amount-1)
+    changeAmount(id, amount - 1)
   }
   const increase = () => {
     console.log('+++++')
-    changeAmount(id, amount+1);
+    changeAmount(id, amount + 1);
   }
-  
-  if (amount>0)
-  return (
-  <div className={styles.container}>
-    <div className={styles.image_container}>
-      <Image
-            className={styles.articleImg}
-            src={image||Imagetmp.src}
-            width={400}
-            height={300}
-            alt={`картинка`}
-            priority
-      />
-    </div>
-      <div className={styles.text_content}>
-        <div className={styles.name_line}>{name}</div>
-        <div className={styles.cost_line}>
-          <div className={cn(styles.def_price, sale&&styles.def_price__saled )}>{cost + currency}</div>
-          {sale&&<div className={styles.sale}>{sale + currency}</div>}
+
+  if (amount > 0)
+    return (
+      <div className={styles.container}>
+        <div className={styles.main_content}>
+          <div className={styles.image_container}>
+            <Image
+              className={styles.articleImg}
+              src={image || Imagetmp.src}
+              width={400}
+              height={300}
+              alt={`картинка`}
+              priority
+            />
+          </div>
+          <div className={styles.text_content}>
+            <div className={styles.name_line}>{name}</div>
+            <div className={styles.cost_line}>
+              <div className={cn(styles.def_price, sale && styles.def_price__saled)}>{cost + currency}</div>
+              {sale && <div className={styles.sale}>{sale + currency}</div>}
+            </div>
+            <div className={styles.amount_line}>
+              <button className={styles.button} onClick={decrease}>-</button>
+              <div className={styles.amount}> {amount}</div>
+              <button className={styles.button} onClick={increase}>+</button>
+            </div>
+          </div>
         </div>
-        <div className={styles.amount_line}>
-            <button onClick={decrease}>-</button>
-            {amount}
-            <button onClick={increase}>+</button>
-        </div>
+        <button className={styles.remove_button} title="Удалить">
+          ×
+        </button>
       </div>
-    </div>
-  )
+    )
   else return <></>
 };
 
-export default  BasketCard;
+export default BasketCard;
